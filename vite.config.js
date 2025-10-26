9a78918ea9f372d278777713e665210c8a8b7281
Anna Kapitanova <totoshechka19@gmail.com>
2025-10-26 21:36:04 +0300

    Настроен универсальный прокси для dev и prod

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -13,6 +12,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://109.73.206.144:6969',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
